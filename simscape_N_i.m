66712377e956f038bf41da7f1e9e8a335a5a39d1
Sena Kotan <senakotan34@gmail.com>
2026-01-10 17:23:10 +0300

    Update simscape_N_i.m

--- simscape_N_i.m
+++ simscape_N_i.m
@@ -1,4 +1,4 @@
-%% Simscape – Hız (N, rpm) ve Akım Grafiği
+%% Simscape – Hız (N, rpm) ve Akım Grafigi
 N_ss_rpm = w_ss * 60 / (2*pi);   % rad/s -> rpm
 
 figure;
@@ -16,6 +16,6 @@ xlabel('Zaman (s)');
 title('Simscape Modeli – N ve Akım');
 legend('Hız (rpm)', 'Armatür Akımı', 'Location', 'best');
 
-% ---- Arka planı beyaz yap ----
-set(gcf, 'Color', 'w');   % Figure arka planı
-set(gca, 'Color', 'w');   % Grafik alanı (axes)
+set(gcf, 'Color', 'w');  
+set(gca, 'Color', 'w');  
+
